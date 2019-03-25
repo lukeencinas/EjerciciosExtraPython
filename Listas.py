3a080a3e133303f618432dcb8a1bc9c8599d57ab
lukeencinas <48908071+lukeencinas@users.noreply.github.com>
2019-03-25 10:45:39 +0100

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,20 @@
+#1) e1 = ["F. de Computadores", "Visualizacion","Fisica","Quimica", "Historia", "Lengua"]
+# print(e1)
+
+#2) e2 = ["F. de Computadores", "Visualizacion","Fisica","Quimica", "Historia", "Lengua"]
+# for i in e2:
+  #  print("Yo estudio: "+i)
+
+#3) e3 = ["F. de Computadores", "Visualizacion","Fisica"]
+# nota = []
+# for i in e3:
+  #  nota.append(input('¿Que has sacado en '+ i + '? '))
+# for i in range(len(e3)):
+ #   print ("En la asignatura ",e3[i]," Has sacado ",nota[i])
+
+
+e4 = []
+for i in range(6):
+    e4.append(int(input("¿Cuales son los numeros de la loteria? ")))
+e4.sort()
+print("Los numeros en orden son: "+ str(e4))
