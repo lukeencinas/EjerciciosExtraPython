13b7f3cf90ed79b87a7615bf21561eca92bc0037
Lucas Encinas <l.encinas@usp.ceu.es>
2019-03-26 16:48:38 +0100

    Añadido bloques 1 y 2

---
+++ Bloque1.py
@@ -0,0 +1,25 @@
+#1) print('Hola Mundo')
+
+#2) a = 'Hola Mundo'
+# print(a)
+
+#3) a = input("Como te llamas? ")
+# print(a)
+
+
+#4) name = input('Como te llamas? ')
+# num = input('Escribe un numero: ')
+# for i in range(int(num)):
+  #  print(name[i], end = '\n')
+
+#5) name = input('Cual es tu nombre? ')
+# print("El nombre "+ name + " tiene "+ str(len(name)) + " letras")
+
+#6) print(((3+2)/(2*5))**2)
+
+#7) altura = float(input('Cuanto mides? '))
+# peso = float(input('Cuanto pesas? '))
+# print((peso/(altura)**2))
+
+
+
---
+++ Bloque2.py
@@ -0,0 +1,26 @@
+#1) edad = int(input('Cual es tu edad? '))
+# if edad > 18: 
+  #  print("Eres mayor de edad")
+# else:
+  #      print("Eres menor de edad")
+
+#2) cs = 'lukeencinas'
+# intento = input("Escribe tu contrasena: ")
+# if intento == cs:
+ #   print("Cs correcta")
+# else:
+ #   print("Cs incorrecta")    
+
+#3) n1 = int(input('Introduce el primer numero '))
+#n2 = int(input('Introduce el segundo numero '))
+#if n1/n2 != 0:
+ #   print(n1/n2)
+#else: 
+ #   print('Error')
+
+#4) edad = int(input('Cual es tu edad? '))
+#salario = int(input('Cuanto cobras al año? '))
+#if edad > 16 and salario > 1000:
+ #   print("Tienes que tributar.")
+#else: 
+ #   print('No tienes que tributar')
