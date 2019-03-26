241acf42bc251fb5b005804f24ac8d57b639baf7
Lucas Encinas <l.encinas@usp.ceu.es>
2019-03-26 19:51:27 +0100

    Añadido problemaas de ListasyTuplas

---
+++ ListasyTuplas.py
@@ -0,0 +1,23 @@
+#word = input("Introduce una palabra: ")
+#reversed_word = word
+#word = list(word)
+#reversed_word = list(reversed_word)
+#reversed_word.reverse()
+#if word == reversed_word:
+#    print("Es un palíndromo")
+#else:
+#    print("No es un palíndromo")
+
+
+#l = ['Bt','Luke','zzz']
+#for ls in l:
+#    print("tengo: "+ ls)
+
+
+asig = ['Mates', 'Frances', 'Lengua', 'Ingles']
+nota = []
+for i in asig:
+    n = input('Que nota has sacado para: '+ i)
+    nota.append(n)
+for i in range(len(asig)):
+    print("Has sacado ", nota[i], " en la asignatura ", asig[i])
