2fbdc2af7eb0da9e42099610ded0c9075fb342d5
Lucas Encinas <l.encinas@usp.ceu.es>
2019-03-26 18:20:39 +0100

    BuclesMejorado

---
+++ BuclesMejorado.py
@@ -0,0 +1,47 @@
+#n1 = input('Cual es tu nombre? ')
+#for i in range(10):
+ #   print(n1)
+
+
+#e1 = int(input('Cual es tu edad? '))
+#for i in range(e1):
+ #   print(i+1)
+
+
+#n2 = int(input('Dame un numero positivo '))
+#for i in range(n2):
+#    if i%2 == 0: 
+#       print(i, end = ', ')
+
+
+#n3 = int(input('Dame un numero positivo '))
+#for i in range(n3,-1,-1):
+#    print(i, end =', ')
+
+
+#amount = float(input('Cantidad a invertir? '))
+#interes = float(input('Tipo de interes? '))
+#tiempo = int(input('Cuanto tiempo? '))
+#for i in range(tiempo):
+#    amount *= 1 + interes / 100
+#    print('Dienro tras: ' + str(i+1) + ' años '+ str(amount) )
+
+
+#n1 = int(input("Dame un numero entero positivo: "))
+#for i in range(n1):
+#    print("*"*int(i+1))
+
+
+#for i in range(1,11):
+#    for j in range(1,11):
+#        print(i*j, end = "\t " )
+
+
+#n = int(input("Introduce la altura del triángulo (entero positivo): "))
+#for i in range(1, n+1, 1):
+#    for j in range(i, 0, -1):
+#        print(j, end=" ")
+#    print("")
+
+
+
